0df34e9907aed19fb68ff8d53aefb79424bbabdf
Monica Mantilla <monicamantilla@macbook-pro-2.home>
2019-07-24 14:49:47 -0400

    fixed routes

--- Guru/src/App.js
+++ Guru/src/App.js
@@ -1,5 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Fixed from "./components/pages/Fixed";
+import Profile from "./components/pages/Profile";
+import Savings from "./components/pages/Savings";
+import Debt from "./components/pages/Debt";
+import Variable from "./components/pages/Variable";
+import NavTabs from "./components/NavTabs"
 
 
 function App() {
@@ -11,6 +18,8 @@ function App() {
           <Route exact path="/fixed" component={Fixed} />
           <Route exact path="/profile" component={Profile} />
           <Route path="/savings" component={Savings} />
+          <Route path="/debt" component={Debt} />
+          <Route path="/variable" component={Variable} />
         </div>
       </Router>
     );
